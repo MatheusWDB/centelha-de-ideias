a3c92656b366a4c56765bab0948779fed19d890c
Matheus Wendell Dantas Bezerra <137704756+MatheusWDB@users.noreply.github.com>
2025-05-17 03:03:05 -0300

    Atualizar o GeminiService.java teste

--- backend/src/main/java/br/com/hematsu/centelha_de_ideias/services/GeminiService.java
+++ backend/src/main/java/br/com/hematsu/centelha_de_ideias/services/GeminiService.java
@@ -14,7 +14,7 @@ import jakarta.annotation.PreDestroy;
 public class GeminiService {
 
     //API KEY nas variáveis de ambiente
-    @Value("${api.key}")
+    @Value("${API_KEY}")
     private String apiKey;
 
     private Models modelsApiClient;
